64eddfa6a85c58dad5518b3c00ff653b6acf2e29
Timo Cramer <timo.cramer@udo.edu>
2024-07-09 14:12:05 +0200

    use better variable names in flash.sh

--- code/flash.sh
+++ code/flash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-OUTPUT_FILENAME=flash_time.bin
+BINARY=binaeruhr.hex
+FLASH_TIME_FILE=flash_time.bin
 
 function dec2hex() {
     printf '%x' "$1"
@@ -11,13 +12,13 @@ function dec2bin() {
 }
 
 function writeCurrentTime() {
-    rm -f "$OUTPUT_FILENAME"
+    rm -f "$FLASH_TIME_FILE"
     
     date '+%-H %-M %-S' | tr ' ' "\n" | while read val; do
-        dec2bin $val >>"$OUTPUT_FILENAME"
+        dec2bin $val >>"$FLASH_TIME_FILE"
     done
 }
 
 writeCurrentTime
 
-avrdude -p m48pa -c usbasp -U flash:w:binaeruhr.hex:i -U eeprom:w:flash_time.bin:r -U lfuse:w:0x62:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+avrdude -p m48pa -c usbasp -U flash:w:${BINARY}:i -U eeprom:w:${FLASH_TIME_FILE}:r -U lfuse:w:0x62:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
